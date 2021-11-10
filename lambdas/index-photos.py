5797c6c49831d8d733dc4e9450eb16059225e31f
Pruthviraj Patil <pruthvir.patil98@gmail.com>
2021-11-10 15:22:15 -0500

    Update index-photos.py

--- lambdas/index-photos.py
+++ lambdas/index-photos.py
@@ -4,33 +4,32 @@ from aws_requests_auth.aws_auth import AWSRequestsAuth
 import time
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 import requests
+import io
+import base64
+from base64 import decodestring
 
 def lambda_handler(event, context):
-    # S3_details=event['Records'][0]['s3']
-    
+    S3_details=event['Records'][0]['s3']
     bucket=event['Records'][0]['s3']['bucket']['name']
     photo=event['Records'][0]['s3']['object']['key']
     timeStamp=time.time()
+    s3_client = boto3.client('s3')
+    s3_clientobj = s3_client.get_object(Bucket=bucket, Key=photo)
+    body=s3_clientobj['Body'].read().decode('utf-8')
+    image = base64.b64decode(body)        
     
     client=boto3.client('rekognition')
-    
-    response = client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}}, MaxLabels=10, MinConfidence=80)   
-        
-    print(response)
-    
+    response = client.detect_labels(
+    Image={'Bytes':image},
+    MaxLabels=10,
+    MinConfidence=80
+    )
     
     labels=response['Labels']
     custom_labels=[]
     for label in labels:
-        if(label['Confidence']>40):
-            custom_labels.append(label['Name'])
+        custom_labels.append(label['Name'])
    
-    photo="abc1.jpg"
-    bucket="pqrs1"
-    custom_labels=['abc', 'pqr']
-    
-    print(custom_labels)
-    
     format={
         'objectKey':photo,
         'bucket':bucket,
@@ -38,7 +37,7 @@ def lambda_handler(event, context):
         'labels':custom_labels
     }
     
-    host="search-picpiper-es-nswdvekospzycu2hyhwmad5aki.us-east-1.es.amazonaws.com"
+    host="search-piper-es-2vi7inr55exmidpivownvkwgqu.us-east-1.es.amazonaws.com"
     
     es_payload=json.dumps(format).encode("utf-8")
     awsauth = AWSRequestsAuth(aws_access_key='AKIAZ43I5RB2K367DHFN',
